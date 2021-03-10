c46c59c82eac8ccfc14612dfdf56cf172c96d015
Mike <pandamayc_20@hotmail.com>
2021-03-10 01:05:26 -0600

    init

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,10 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [{ path: 'hero', loadChildren: () => import('./hero/hero.module').then(m => m.HeroModule) }];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
