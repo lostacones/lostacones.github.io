d8eaa7ced6d076a838f6ffba2b4fce3d0f62dbd6
Mike <pandamayc_20@hotmail.com>
2021-03-10 02:06:34 -0600

    Adding first components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HeroComponent } from './hero/hero.component';
 
-const routes: Routes = [{ path: 'hero', loadChildren: () => import('./hero/hero.module').then(m => m.HeroModule) }];
+const routes: Routes = [
+
+  { path: '', component: HeroComponent },
+
+  { path: 'about', loadChildren: () => import('./about/about.module').then(m => m.AboutModule) },
+
+  { path: 'contact', loadChildren: () => import('./contact/contact.module').then(m => m.ContactModule) }
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/hero/hero.component.ts
+++ src/app/hero/hero.component.ts
@@ -7,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeroComponent implements OnInit {
 
+  images: Array<string> = [ './assets/logo.jpeg' ];
+
   constructor() { }
 
   ngOnInit(): void {
